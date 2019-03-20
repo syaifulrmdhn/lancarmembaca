dbcf1d8455c149bdadfa00160a94eca5c8fcb528
syaifulrmdhn <syaifrmdhn@gmail.com>
2019-03-20 17:27:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="eng" class="no-js">
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/book.png">
+	<!-- Author Meta -->
+	<meta name="author" content="Eko Sartono">
+	<!-- Meta Description -->
+	<meta name="description" content="Buku ini dapat dijadikan materi ajar yang dapat membantu orangtua dan guru dalam memunculkan kecerdasan membaca siswa disajikan dengan Lengkap,Sistematis, Kualitas Teruji">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="belajar, belajar membaca, membaca, lancar membaca, cerdas, siswa, buku">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Lancar Membaca</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,500,600" rel="stylesheet">
+		<!--
+		CSS
+		============================================= -->
+		<link rel="stylesheet" href="css/linearicons.css">
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/nice-select.css">
+		<link rel="stylesheet" href="css/magnific-popup.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<div class="main-wrapper-first">
+			<div class="hero-area relative">
+				<header>
+					<div class="container">
+						<div class="header-wrap">
+							<div class="header-top d-flex justify-content-between align-items-center">
+								<div class="logo">
+									<a href="index.html"><img src="img/logo.png" alt="logo"></a>
+								</div>
+								<div class="main-menubar d-flex align-items-center">
+									<nav class="hide">
+										<a href="index.html">Home</a>
+										<!-- <a href="generic.html">Generic</a>
+										<a href="elements.html">Elements</a> -->
+									</nav>
+									<div class="menu-bar"><span class="lnr lnr-menu"></span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</header>
+				<div class="banner-area">
+					<div class="container">
+						<div class="row height align-items-center">
+							<div class="col-lg-6">
+								<div class="banner-content">
+									<h1 class="text-white text-uppercase mb-10">Buku ini dapat dijadikan <br> materi ajar </h1>
+									<p class="text-white mb-30">yang dapat membantu orangtua dan guru dalam memunculkan kecerdasan membaca siswa disajikan dengan Lengkap, Sistematis, Kualitas Teruji</p>
+									<a href="#" class="primary-btn d-inline-flex align-items-center"><span class="mr-10">Kontak</span><span class="lnr lnr-arrow-right"></span></a>
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<img src="img/buku.png" style="max-width:100%; max-height:100%;" alt="buku">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="main-wrapper">
+			<!-- Start Remarkable Wroks Area -->
+			<section class="remarkable-area">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-6">
+							<div class="section-title text-center">
+								<h2>Kenapa ?</h2>
+								<p>Siapa yang tidak ingin ananda tercinta atau anak didiknya dapat membaca? Keterampilan membaca merupakan pintu bagi ananda untuk dapat membuka ilmu pengetahuan, baik tentang dirinya maupun tentang alam semesta.</p>
+							</div>
+						</div>
+					</div>
+					<div class="single-remark">
+						<div class="row no-gutters">
+							<div class="col-lg-4 col-md-5">
+								<div class="remark-thumb" style="background: url(img/horizontal.png);"></div>
+							</div>
+							<div class="col-lg-8 col-md-7">
+								<hr>
+								<div class="story-box">
+									<p>Ada banyak metode membaca, di antaranya metode fonik, metode silabi, metode pengalaman berbahasa, dan metode kartu. Semua motode tersebut dapat digunakan sesuai dengan karakteristik peserta didik, bahasa yang dipelajari, waktu yang sesuai, serta ketepatan pembelajarannya.</p>
+									<p>Metode cepat 50 Jam pintar membaca dirancang sedemikian rupa untuk anak usia 4--6 tahun yang ingin bisa membaca dalam bahasa Indonesia. Pertimbangan karakteristik anak, karakteristik bahasa Indonesia, alokasi waktu pembelajaran, serta ketepatan pembelajaran diarahkan sedemikian rupa untuk mencapai tingkat keberhasilan tinggi bagi anak-anak Indonesia yang akan belajar membaca.</p>
+									<p>Tentu saja setelah kemampuan membaca telah dimiliki sang anak, perlu tindak lanjut untuk mengarahkan anak agar dapat menyukai bahkan mencintai kegiatan membaca. Hmm, itu tugas orang tua dan guru selanjutnya.</p>
+									<p>Percayalah, Anda akan kaget dan tidak percaya bahwa begitu anak atau peserta didik Anda menyukai kegiatan membaca, tingkat intelegensinya berkembang dengan cepat. Ia pun lebih siap untuk mempelajari semua pengetahuan sebelum nanti ketika dewasa ia menetapkan satu pengetahuan khusus sebagai bekal kecakapan hidupnya.</p>
+									<p>Anda berminat untuk berinvestasi dalam menciptakan generasi Indonesia yang gemar membaca? Kabarkanlah buku ini kepada mereka yang membutuhkan. Terima kasih. </p>
+								</div>
+								<hr>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End Remarkable Wroks Area -->
+			<!-- Start Contact Form 
+			<section class="contact-form-area">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-6">
+							<div class="section-title text-center">
+								<h2 class="text-white">Keep in Touch</h2>
+								<p class="text-white">Most people who work in an office environment, buy computer products, or have a computer at home have had the “fun” experience of dealing </p>
+							</div>
+						</div>
+					</div>
+					<form id="myForm" action="mail.php" method="post" class="contact-form">
+						<div class="row justify-content-center">
+							<div class="col-lg-5">
+								<input type="text" name="fname" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" class="common-input mt-20" required>
+							</div>
+							<div class="col-lg-5">
+								<input type="email" name="email" placeholder="Enter email address" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="common-input mt-20" required>
+							</div>
+							<div class="col-lg-10">
+								<textarea class="common-textarea mt-20" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required></textarea>
+							</div>
+							<div class="col-lg-10 d-flex justify-content-end">
+								<button class="primary-btn submit-btn d-inline-flex align-items-center mt-20"><span class="mr-10">Send Message</span><span class="lnr lnr-arrow-right"></span></button> <br>
+								<div class="alert-msg"></div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</section>
+			<!-- End Contact Form -->
+			<!-- Start Footer Widget Area 
+			<section class="footer-widget-area">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="single-widget">
+								<div class="desc">
+									<h6 class="title">Address</h6>
+									<p>56/8, panthapath, west <br> dhanmondi, kalabagan</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="single-widget">
+								<div class="desc">
+									<h6 class="title">Email Address</h6>
+									<div class="contact">
+										<a href="mailto:info@dataarc.com">info@dataarc.com</a> <br>
+										<a href="mailto:support@dataarc.com">support@dataarc.com</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="single-widget">
+								<div class="desc">
+									<h6 class="title">Phone Number</h6>
+									<div class="contact">
+										<a href="tel:1545">012 4562 982 3612</a> <br>
+										<a href="tel:54512">012 6321 956 4587</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<footer>
+					<div class="container">
+						<div class="footer-content d-flex justify-content-between align-items-center flex-wrap">
+							<div class="logo">
+								<a href="index.html"><img src="img/logo.png" alt=""></a>
+							</div>
+							<div class="copy-right-text">Copyright &copy; 2017  |  All rights reserved to <a href="#">Dinomuz inc.</a> Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></div>
+							<div class="footer-social">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-dribbble"></i></a>
+								<a href="#"><i class="fa fa-behance"></i></a>
+							</div>
+						</div>
+					</div>
+				</footer>
+			</section>
+			<!-- End Footer Widget Area -->
+
+		</div>
+
+
+
+
+		<script src="js/vendor/jquery-2.2.4.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="js/vendor/bootstrap.min.js"></script>
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.nice-select.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
